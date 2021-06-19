3bebede07ffd3e16632776e1423d7a4a0b92a6a8
Son Dang <sondnpt00343@gmail.com>
2021-06-19 22:54:42 +0700

    Ví dụ lấy data từ API thuộc hệ thống khác lưu vào DB

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const dbConfig = require('../config/db')
 
-async function connect() {
+async function connect(onConnected) {
     const { host, port, dbname } = dbConfig
 
     try {
@@ -12,6 +12,8 @@ async function connect() {
             useCreateIndex: true
         });
         console.log('Connect to DB successfully!')
+        if (typeof onConnected === 'function')
+            onConnected()
     } catch (error) {
         console.log('Connect to DB failure!')
     }
